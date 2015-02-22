66356445752b40904644e07aef3cae12087e5aad
Admin <Admin@Admin-PC>
2015-02-21 22:47:05 -0300

    prueba inicio sesion usuario

---
+++ application/views/usuario_view.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        
+        <?=$this->session->userdata('usuario')?>
+        <a href="<?php base_url();?>login/cerrar_sesion">cerrar sesion</a>
+
+    </body>
+</html>
